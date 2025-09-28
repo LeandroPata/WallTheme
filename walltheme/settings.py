508d5d4bd4778e2c4ad88bcf4ebfc49b05c41564
LeandroPata <lmfpata@gmail.com>
2025-09-28 18:16:18 +0100

    Added a MODULE_DIR

--- walltheme/settings.py
+++ walltheme/settings.py
@@ -9,3 +9,4 @@ __version__ = '0.1.7'
 
 TEMPLATE_DIR = Path(os.path.expanduser('~/.config/walltheme/templates'))
 CACHE_DIR = Path(os.path.expanduser('~/.cache/walltheme'))
+MODULE_DIR = Path(os.path.dirname(__file__))
